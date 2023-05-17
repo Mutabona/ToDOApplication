05e1c6cce315f19a10aba265c848f52e85a8eac5
Mutabona <k_dod1@mail.ru>
2023-05-17 19:56:06 +0300

    try

---
+++ src/main/java/ToDoApp/ToDoApplication/models/User.java
@@ -0,0 +1,54 @@
+package ToDoApp.ToDoApplication.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Size(min = 2, message = "Не меньше 5 знаков")
+    private String username;
+    @Size(min = 1, message = "Не меньше 5 знаков")
+    private String password;
+    @Transient
+    private String passwordConfirm;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/ToDoApp/ToDoApplication/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package ToDoApp.ToDoApplication.repositories;
+
+import ToDoApp.ToDoApplication.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
